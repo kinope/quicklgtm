91b0d1085ebb3c3258bec3330235cdc874e0460c
kinope <kino8810@gmail.com>
2017-09-23 17:40:15 +0900

    コメントの内容を変更できるようにした

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,19 +1,38 @@
 'use strict';
 
-let LGTM = '👍';
+let defaultLGTM = '👍';
+let lgtm = '';
 
 window.onload = function () {
+
+  chrome.storage.sync.get('lgtm_word', function(v) {
+    let word = v.lgtm_word;
+    lgtm = word == undefined ? defaultLGTM : word;
+    displayLGTMButton(lgtm)
+  });
+
+  chrome.storage.onChanged.addListener(function(objects, areaName){
+    console.log(objects);
+    if (areaName == 'sync') {
+      let new_word = objects.lgtm_word.newValue;
+      document.querySelector('.btn.lgtm-button').textContent = new_word;
+      console.log(new_word);
+    }
+  });
+
+};
+
+function displayLGTMButton(lgtm) {
   let formBox = document.querySelector('.js-new-comment-form .form-actions');
   let textArea = document.getElementById('new_comment_field');
   let parentButtonBox = document.getElementById('partial-new-comment-form-actions');
 
   const button = document.createElement('BUTTON');
-  button.setAttribute('class', 'btn');
-  button.innerHTML = LGTM;
+  button.setAttribute('class', 'btn lgtm-button');
+  button.innerHTML = lgtm;
   button.addEventListener('click', function(){
-    textArea.value = LGTM;
+    textArea.value = lgtm;
   }, false);
 
   parentButtonBox.appendChild(button);
-
-};
+}
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+document.addEventListener('DOMContentLoaded', function(e) {
+  window.onload = function() {
+    let submitButton = document.getElementById('submit-button');
+    submitButton.disabled = true;
+    let textArea = document.getElementById('lgtm-word');
+
+    textArea.addEventListener('input', function (e) {
+      displaySubmit(submitButton, textArea.value);
+    }, false);
+    submitButton.addEventListener('click', function(){
+      let inputWord = textArea.value;
+      chrome.storage.sync.set({'lgtm_word': inputWord});
+    }, false);
+  }
+});
+
+function displaySubmit(submit, val) {
+  if (val == '') {
+    submit.disabled = true;
+  } else {
+    submit.disabled = false;
+  }
+}
